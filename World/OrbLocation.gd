87e630f1e06a66822a1bbe06b60c07db54dbcdf7
Unknown <LJPayne98@gmail.com>
2018-11-10 22:22:32 +0000

    Orb functionality
    - Added orbs spawn to the game
    - Orbs re spawn after a given time (atm its 2 seconds)

--- World/OrbLocation.gd
+++ World/OrbLocation.gd
@@ -2,7 +2,16 @@ extends Node2D
 
 export(PackedScene) var orb_scene = preload("res://World/Buff/Buff.tscn")
 
+var orb = null
+
 func _on_OrbTimer_timeout():
-	var orb = orb_scene.instance()
+	if (orb != null):
+		orb.queue_free()
+	
+	orb = orb_scene.instance()
 	orb.set_position(self.get_position())
 	get_tree().get_root().add_child(orb)
+
+func _check_timer():
+	
+	
